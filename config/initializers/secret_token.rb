72643900cc293419b86da8cd7427353559c86e90
Genki Ishibashi <b0941015@gmail.com>
2014-01-12 18:31:37 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studyinturn2::Application.config.secret_key_base = '40d1ac1e83ade921f9ec863f470c161fb44740bedcacbf3f9def182daad86a8c0286ab7f15dfbef7ea6858b8c86504ebc7f1e4233a633fe84fe9236df3dce1c0'
